275faedcf14df44af84b7bee51c596dfa73d6506
faizparvez <151218353+faizparvez@users.noreply.github.com>
2024-10-21 19:56:25 +0530

    added button

--- Frontend/src/components/design/Pricing.jsx
+++ Frontend/src/components/design/Pricing.jsx
@@ -2,7 +2,7 @@ import { lines } from "../../assets";
 
 export const LeftLine = () => {
   return (
-    <div className="hidden lg:block absolute top-1/2 right-full w-[92.5rem] h-[11.0625rem] -translate-y-1/2 pointer-events-none">
+    <div className="hidden lg:block absolute top-1/2 right-full w-[92.5rem] h-[11.0625rem] -translate-y-1/2 pointer-events-none border-6">
       <img
         className="w-full"
         src={lines}
